7b2e341be8bfa617050725393a6d5af037b03f8f
Alex Pleava <pleava1@mcmaster.ca>
2020-07-10 15:42:32 -0400

    Made RandomComparisons.py look at every 10th comparison (07/10/2020-3:42)

--- DataProcessing/RandomComparisons_V4.py
+++ DataProcessing/RandomComparisons_V4.py
@@ -48,50 +48,57 @@ if __name__ == "__main__":
     chains_directory = os.path.join(root_directory, "..", "NonRedundant_Chains")
     output_file_path = os.path.join(root_directory, "..", "RandomComparisons.csv")
     with open(output_file_path, "w") as output_file:
-        for species_folder in os.listdir(root_directory):
-            assemblies_directory = os.path.join(root_directory, species_folder, "Selected_Biological_Assemblies")
-            temp_path = os.path.join(root_directory, species_folder, "_Temp")
-            
+        temp_path = os.path.join(root_directory, "_Temp")
+        if not os.path.exists(temp_path): os.mkdir(temp_path)
 
-            chain_files = os.listdir(chains_directory)
-            chain_names = [cn.split(".")[0] for cn in chain_files]
-            counter = 0
-            pairings_examined = 0
-            output_file.write("PDB_ID_1,PDB_ID_2,Chain_1,Chain_2,Chain_1_Length,Chain_2_Length,Alignment_Score,PID,TM\n")
-            for chain_idx_a in range(len(chain_names)):
-                for chain_idx_b in range(chain_idx_a):
+        # Get the list of chains that we need to compare
+        chain_files = os.listdir(chains_directory)
+        chain_names = [cn.split(".")[0] for cn in chain_files]
 
-                    if pairings_examined >= n_pairings: 
-                        sys.exit()
+        # Counter so we know what number comparison this is, and how many pairings we've already done
+        counter = 0
+        pairings_examined = 0
 
+        # Prepare the output file
+        output_file.write("PDB_ID_1,PDB_ID_2,Chain_1,Chain_2,Chain_1_Length,Chain_2_Length,Alignment_Score,PID,TM\n")
+
+        # Go through each combination of the chains
+        for chain_idx_a in range(len(chain_names)):
+            for chain_idx_b in range(chain_idx_a):
+                
+                # If we have enough chains, quit
+                if pairings_examined >= n_pairings: 
+                    sys.exit()
+
+                # If this is the tenth pairing to be considered, then compare them
+                if counter%10 == 0:
+                    # Count the pairing
+                    pairings_examined += 1
+                    # Do the sequence and structure comparisons
                     pdb_id_a = chain_names[chain_idx_a][:4].lower()
                     chain_letter_a = chain_names[chain_idx_a][5].upper()
                     chain_a_filename = f"{pdb_id_a}_{chain_letter_a}.ent"
                     pdb_a_filename = f"{pdb_id_a}.cif"
-
                     pdb_id_b = chain_names[chain_idx_b][:4].lower()
                     chain_letter_b = chain_names[chain_idx_b][5].upper()
                     chain_b_filename = f"{pdb_id_b}_{chain_letter_b}.ent"
                     pdb_b_filename = f"{pdb_id_b}.cif"
+                    print(f"Processing: {chain_a_filename} vs {chain_b_filename} # {pairings_examined}")
+                    # Get the paths to the chains, and the overall complex
+                    chain_a_path = os.path.join(chains_directory, f"{pdb_id_a}_{chain_letter_a}.ent")
+                    chain_b_path = os.path.join(chains_directory, f"{pdb_id_b}_{chain_letter_b}.ent")
+                    # Percent identity
+                    nw_score, percent_identity = calculate_percent_identity(f"{pdb_id_a}_{chain_letter_a}", chain_a_path, f"{pdb_id_b}_{chain_letter_b}", chain_b_path, os.path.join(temp_path, "Global_Alignment.txt"))
+                    # TM-Align Score
+                    structure_similarity = calculate_TMScore(chain_a_path, chain_b_path, os.path.join(temp_path, "TMAlign_Output.txt"), alignment=os.path.join(temp_path, "Global_Alignment.txt"))
+                    # Save the data
+                    line = f"{pdb_id_a},{pdb_id_b},{chain_letter_a},{chain_letter_b},{len(load_sequence(chain_a_path))},{len(load_sequence(chain_b_path))},{nw_score},{percent_identity},{structure_similarity}"
+                    print("    "+line)
+                    output_file.write(line + "\n")
 
-                    if counter%1 == 0: # If the files exist, and this is the hundredth pairing examined, then analyze
-                        pairings_examined += 1
-                        print(f"Processing: {chain_a_filename} vs {chain_b_filename} # {pairings_examined}")
-                        # Get the paths to the chains, and the overall complex
-                        chain_a_path = os.path.join(chains_directory, f"{pdb_id_a}_{chain_letter_a}.ent")
-                        chain_b_path = os.path.join(chains_directory, f"{pdb_id_b}_{chain_letter_b}.ent")
-                        # Percent identity
-                        nw_score, percent_identity = calculate_percent_identity(f"{pdb_id_a}_{chain_letter_a}", chain_a_path, f"{pdb_id_b}_{chain_letter_b}", chain_b_path, os.path.join(temp_path, "Global_Alignment.txt"))
-                        # TM-Align Score
-                        structure_similarity = calculate_TMScore(chain_a_path, chain_b_path, os.path.join(temp_path, "TMAlign_Output.txt"), alignment=os.path.join(temp_path, "Global_Alignment.txt"))
-                        # Save the data
-                        line = f"{pdb_id_a},{pdb_id_b},{chain_letter_a},{chain_letter_b},{len(load_sequence(chain_a_path))},{len(load_sequence(chain_b_path))},{nw_score},{percent_identity},{structure_similarity}"
-                        print("    "+line)
-                        output_file.write(line + "\n")
-
-                    
-                    # Increment our counter
-                    counter += 1
+                
+                # Increment our counter
+                counter += 1
 
 
                     
