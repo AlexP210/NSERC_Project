6e25cf3f21b8561b0615b3ce9178ee88aa59d5ee
Alex Pleava <pleava1@mcmaster.ca>
2020-07-15 16:26:18 -0400

    Fixed Random Comparisons (07/15/20-4:26)

--- DataProcessing/RandomComparisons_V4.py
+++ DataProcessing/RandomComparisons_V4.py
@@ -12,6 +12,7 @@ import sys
 from Bio.SubsMat import MatrixInfo as matlist
 from Bio.pairwise2 import align, format_alignment
 from My_Library import *
+import random as rand
 
 # def load_sequence(pdb_id, chain, directory):
 
@@ -62,43 +63,35 @@ if __name__ == "__main__":
         # Prepare the output file
         output_file.write("PDB_ID_1,PDB_ID_2,Chain_1,Chain_2,Chain_1_Length,Chain_2_Length,Alignment_Score,PID,TM\n")
 
+        compared = set()
         # Go through each combination of the chains
-        for chain_idx_a in range(len(chain_names)):
-            for chain_idx_b in range(chain_idx_a):
-                
-                # If we have enough chains, quit
-                if pairings_examined >= n_pairings: 
-                    sys.exit()
+        for i in range(10000):
+            chain_idx_a = rand.randint(0, len(chain_names)-1)
+            chain_idx_b = rand.randint(0, len(chain_names)-1)
+            while chain_idx_a == chain_idx_b:
+                chain_idx_a = rand.randint(0, len(chain_names)-1)
+                chain_idx_b = rand.randint(0, len(chain_names)-1)
 
-                # If this is the tenth pairing to be considered, then compare them
-                if counter%10 == 0:
-                    # Count the pairing
-                    pairings_examined += 1
-                    # Do the sequence and structure comparisons
-                    pdb_id_a = chain_names[chain_idx_a][:4].lower()
-                    chain_letter_a = chain_names[chain_idx_a][5].upper()
-                    chain_a_filename = f"{pdb_id_a}_{chain_letter_a}.ent"
-                    pdb_a_filename = f"{pdb_id_a}.cif"
-                    pdb_id_b = chain_names[chain_idx_b][:4].lower()
-                    chain_letter_b = chain_names[chain_idx_b][5].upper()
-                    chain_b_filename = f"{pdb_id_b}_{chain_letter_b}.ent"
-                    pdb_b_filename = f"{pdb_id_b}.cif"
-                    print(f"Processing: {chain_a_filename} vs {chain_b_filename} # {pairings_examined}")
-                    # Get the paths to the chains, and the overall complex
-                    chain_a_path = os.path.join(chains_directory, f"{pdb_id_a}_{chain_letter_a}.ent")
-                    chain_b_path = os.path.join(chains_directory, f"{pdb_id_b}_{chain_letter_b}.ent")
-                    # Percent identity
-                    nw_score, percent_identity = calculate_percent_identity(f"{pdb_id_a}_{chain_letter_a}", chain_a_path, f"{pdb_id_b}_{chain_letter_b}", chain_b_path, os.path.join(temp_path, "Global_Alignment.txt"))
-                    # TM-Align Score
-                    structure_similarity = calculate_TMScore(chain_a_path, chain_b_path, os.path.join(temp_path, "TMAlign_Output.txt"), alignment=os.path.join(temp_path, "Global_Alignment.txt"))
-                    # Save the data
-                    line = f"{pdb_id_a},{pdb_id_b},{chain_letter_a},{chain_letter_b},{len(load_sequence(chain_a_path))},{len(load_sequence(chain_b_path))},{nw_score},{percent_identity},{structure_similarity}"
-                    print("    "+line)
-                    output_file.write(line + "\n")
-
-                
-                # Increment our counter
-                counter += 1
+            pdb_id_a = chain_names[chain_idx_a][:4].lower()
+            chain_letter_a = chain_names[chain_idx_a][5].upper()
+            chain_a_filename = f"{pdb_id_a}_{chain_letter_a}.ent"
+            pdb_a_filename = f"{pdb_id_a}.cif"
+            pdb_id_b = chain_names[chain_idx_b][:4].lower()
+            chain_letter_b = chain_names[chain_idx_b][5].upper()
+            chain_b_filename = f"{pdb_id_b}_{chain_letter_b}.ent"
+            pdb_b_filename = f"{pdb_id_b}.cif"
+            print(f"Processing: {chain_a_filename} vs {chain_b_filename} # {pairings_examined}")
+            # Get the paths to the chains, and the overall complex
+            chain_a_path = os.path.join(chains_directory, f"{pdb_id_a}_{chain_letter_a}.ent")
+            chain_b_path = os.path.join(chains_directory, f"{pdb_id_b}_{chain_letter_b}.ent")
+            # Percent identity
+            nw_score, percent_identity = calculate_percent_identity(f"{pdb_id_a}_{chain_letter_a}", chain_a_path, f"{pdb_id_b}_{chain_letter_b}", chain_b_path, os.path.join(temp_path, "Global_Alignment.txt"))
+            # TM-Align Score
+            structure_similarity = calculate_TMScore(chain_a_path, chain_b_path, os.path.join(temp_path, "TMAlign_Output.txt"), alignment=os.path.join(temp_path, "Global_Alignment.txt"))
+            # Save the data
+            line = f"{pdb_id_a},{pdb_id_b},{chain_letter_a},{chain_letter_b},{len(load_sequence(chain_a_path))},{len(load_sequence(chain_b_path))},{nw_score},{percent_identity},{structure_similarity}"
+            print("    "+line)
+            output_file.write(line + "\n")
 
 
                     
